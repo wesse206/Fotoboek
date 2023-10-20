5a0f71f7cc8c352b0d7d9586ef3cce0efda264f9
wesse306 <wessel.steenkamp@gmail.com>
2023-10-21 00:48:37 +0200

    First Files :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { NgOptimizedImage } from '@angular/common'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jaarboek';
+  FotoBoek: any[] = [
+    ['1', '3'],
+    []
+  ]
+  StockFotos: any[] = [
+    [false, false],
+    []
+  ]
+
+  showModal = 'none'
+  fotos = new FormControl('')
+  page = 0
+  addImage(page: number) {
+      this.showModal = 'block'
+      this.page = page
+  }
+  removeImage(page: number, image: string) {
+    const index = this.FotoBoek[page].indexOf(image, 0)
+    this.FotoBoek[page].splice(index, 1)
+    this.StockFotos[page].splice(index, 1)
+  }
+  newImage(fotoNommer: string) {
+    this.FotoBoek[this.page].push(fotoNommer)
+    this.StockFotos[this.page].push(false)
+    this.showModal = 'none'
+  }
+
+  imageSrc: any
+  newCustomImage(inputFile: any) {
+    var input: File = inputFile.files[0]
+    const reader = new FileReader();
+    reader.onload = e => {
+      this.imageSrc = reader.result 
+      this.FotoBoek[this.page].push(reader.result)
+    }
+    reader.readAsDataURL(input)
+    
+
+    this.StockFotos[this.page].push(true)
+
+  }
+
+  addPage() {
+    this.FotoBoek.push([])
+    this.StockFotos.push([])
+  }
+  removePage(page:number) {
+    this.FotoBoek.splice(page, 1)
+    this.StockFotos.splice(page, 1)
+  }
 }
+
+
