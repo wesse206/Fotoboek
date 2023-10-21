2ad51f630bca0f62d465bdc8a9b2666acc387468
wesse306 <wessel.steenkamp@gmail.com>
2023-10-21 21:43:59 +0200

    Microsoft login works!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {MsalModule, MsalRedirectComponent, MsalGuard, MsalInterceptor,} from "@azure/msal-angular"; // Import MsalInterceptor
-import {InteractionType, PublicClientApplication} from "@azure/msal-browser";
+import {MsalModule, MsalRedirectComponent, MsalGuard, MsalInterceptor, MsalInterceptorConfiguration, MSAL_INSTANCE, MSAL_INTERCEPTOR_CONFIG, MsalService,} from "@azure/msal-angular"; // Import MsalInterceptor
+import {InteractionType, PublicClientApplication, IPublicClientApplication, BrowserCacheLocation} from "@azure/msal-browser";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: "ae87628e-acd1-414d-b004-275ffe43e2d1",
+      authority: "https://login.microsoftonline.com/4073cc9b-b7f3-4d1c-bac6-19cbe891b0a2",
+      redirectUri: 'http://localhost:4200',
+      postLogoutRedirectUri: 'http://localhost:4200'
+    },
+    cache: {
+      cacheLocation: BrowserCacheLocation.LocalStorage
+    },
+    system: {
+      allowNativeBroker: false, // Disables WAM Broker
+    }
+  });
+}
+
+export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, Array<string>>();
+  protectedResourceMap.set('https://graph.microsoft.com/v1.0/me', ["user.read"]);
+
+  return {
+    interactionType: InteractionType.Redirect,
+    protectedResourceMap
+  };
+}
 
 @NgModule({
   declarations: [
@@ -18,33 +45,8 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    MsalInterceptor,
-    MsalModule.forRoot(
-      new PublicClientApplication({
-        auth: {
-          clientId: "ae87628e-acd1-414d-b004-275ffe43e2d1", // Application (client) ID from the app registration
-          authority:
-            "https://login.microsoftonline.com/4073cc9b-b7f3-4d1c-bac6-19cbe891b0a2", // The Azure cloud instance and the app's sign-in audience (tenant ID, common, organizations, or consumers)
-          redirectUri: "http://localhost:4200", // This is your redirect URI
-        },
-        cache: {
-          cacheLocation: "localStorage",
-        },
-        
-      }),
-      {
-        interactionType: InteractionType.Redirect,
-        authRequest: {
-          scopes: ["user.read"],
-        },
-      },
-      {
-        interactionType: InteractionType.Redirect, // MSAL Interceptor Configuration
-        protectedResourceMap: new Map([
-          ["https://graph.microsoft.com/v1.0/me", ["user.read"]],
-        ]),
-      }
-    ),
+    MsalModule
+
   ],
   providers: [
     {
@@ -52,6 +54,15 @@ import { LoginComponent } from './login/login.component';
       useClass: MsalInterceptor,
       multi: true,
     },
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory
+    },
+    MsalService,
   ],
   bootstrap: [LoginComponent, AppComponent, MsalRedirectComponent]
 })
